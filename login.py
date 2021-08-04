3a2d66ff4e2ef8cc7566e5065f0744c69aef8a9e
nasramin <85575403+nasramin@users.noreply.github.com>
2021-08-04 10:29:15 -0400

    Add files via upload

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 
 #initialize web driver
@@ -14,8 +15,8 @@ logInSuccess = False
 while logInSuccess is False:
 
     #input username and password
-    userName = input('username: ')
-    userName = userName + "@usf.edu"
+    userName = input('username: ') + "@usf.edu"
+    #userName = userName + "@usf.edu"
     passWord = input('password: ')
 
     #retrieve usf website
@@ -64,7 +65,6 @@ phoneCode = False
 while phoneCode is False:
     try:
         verCode = input('Phone code: ')
-        time.sleep(5)
 
         codeBar = driver.find_element_by_id('idTxtBx_SAOTCC_OTC')
         codeBar.send_keys(verCode)
@@ -81,3 +81,43 @@ while phoneCode is False:
         phoneNext.click()
     except:
         print('Phone verification unsuccessful. Try again.')
+
+#accessing oasis
+
+print('Accessing OASIS...')
+driver.get('https://usfonline.admin.usf.edu/pls/prod/bwckschd.p_disp_dyn_sched')
+
+#accessing class search for fall2021
+try:
+    driver.find_element_by_xpath('/html/body/div[3]/form/table/tbody/tr/td/select/option[2]').click()
+    driver.find_element_by_xpath('/html/body/div[3]/form/input[2]').click()
+except:
+    print('Problem accessing class schedule search.')
+
+#input desired subject
+
+classSelected = False
+
+while classSelected is False:
+    try:
+        classSubject = input('Enter desired 3 letter subject code: ').upper()
+        print(classSubject)
+        subjectList = Select(driver.find_element_by_xpath('/html/body/div[3]/form/table[1]/tbody/tr/td[2]/select'))
+        subjectList.select_by_value(classSubject)
+
+        classSelected = True
+    except:
+        print('Class subject selection unsuccessful. Try again')
+
+courseNumberInput = False
+
+while courseNumberInput is False:
+    try:
+        courseNumber = input('Enter desired course number: ')
+        driver.find_element_by_id('crse_id').send_keys(courseNumber)
+
+        driver.find_element_by_xpath('/html/body/div[3]/form/input[12]').click()
+
+        courseNumberInput = True
+    except:
+        print('Course number input unsuccessful, try again.')
